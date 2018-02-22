133422932454d6ea35bb614e8ecfef58ba6037ff
ala_ropat@epam.com <Life45Dream!>
2018-02-22 10:09:25 +0300

    done

--- src/index.js
+++ src/index.js
@@ -5,11 +5,20 @@
 module.exports = function getLoveTrianglesCount(preferences = []) {
     var count = 0;
 
-    for (var i = 0; i <= preferences.length; i++) {
+    for (var i = 0; i <= preferences.length - 1; i++) {
         var n = i + 1;
         var k = preferences[i];
-        if (n === k) continue;
+        if (k === n) continue;
         if (k > preferences.length) continue;
+        if (k === preferences[i + 1]) continue;
+        var first = k - 1;
+        if (first !== i) {
+            first = preferences [first] - 1;
+            if (preferences[first] - 1 === i) {
+                count++;
+                preferences[i] = 0
+            }
+        }
 
 
     }
